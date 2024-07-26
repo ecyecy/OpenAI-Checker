4ac83fd8cc32aded61995a89b90c0119c288d2cb
ecyecy <168642285+ecyecy@users.noreply.github.com>
2024-07-27 06:18:11 +0800

    Update openai.sh

--- openai.sh
+++ openai.sh
@@ -5,7 +5,6 @@
 # @LastEditors: ecyecy
 # @LastEditTime: 2024-07-22 21:21:00
 # @FilePath: /OpenAI-Checker/openai.sh
-# @Telegram: https://t.me/missuo
 # 
 # Copyright © 2023 by Vincent, All Rights Reserved.
 ###
@@ -18,7 +17,7 @@ BLUE="\033[36m"
 
 SUPPORT_COUNTRY=(AL DZ AD AO AG AR AM AU AT AZ BS BD BB BE BZ BJ BT BO BA BW BR BN BG BF CV CA CL CO KM CG CR CI HR CY CZ DK DJ DM DO EC SV EE FJ FI FR GA GM GE DE GH GR GD GT GN GW GY HT VA HN HU IS IN ID IQ IE IL IT JM JP JO KZ KE KI KW KG LV LB LS LR LI LT LU MG MW MY MV ML MT MH MR MU MX FM MD MC MN ME MA MZ MM NA NR NP NL NZ NI NE NG MK NO OM PK PW PS PA PG PY PE PH PL PT QA RO RW KN LC VC WS SM ST SN RS SC SL SG SK SI SB ZA KR ES LK SR SE CH TW TZ TH TL TG TO TT TN TR TV UG UA AE GB US UY VU ZM)
 echo -e "${BLUE}OpenAI Access Checker. Made by Vincent${PLAIN}"
-echo -e "${BLUE}https://github.com/missuo/OpenAI-Checker${PLAIN}"
+echo -e "${BLUE}https://github.com/ecyecy/OpenAI-Checker${PLAIN}"
 echo "-------------------------------------"
 
 check_additional_endpoints() {
